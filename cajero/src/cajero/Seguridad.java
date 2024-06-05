1a7dd7f578d04c0e4882f28cb1276f4f1a755578
Adru <163925265+adrufs@users.noreply.github.com>
2024-06-05 11:36:48 -0400

    Update Seguridad.java

--- cajero/src/cajero/Seguridad.java
+++ cajero/src/cajero/Seguridad.java
@@ -5,17 +5,30 @@ import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
 
+/**
+ * La clase Seguridad proporciona métodos para cifrar y descifrar datos utilizando el algoritmo AES.
+ */
 public class Seguridad {
 
-    private static final String ALGORITMO= "AES";
-    
+    private static final String ALGORITMO = "AES";
     private static final String CLAVE_SECRETA = "miclavesecreta12";
 
+    /**
+     * Obtiene la clave secreta para cifrar y descifrar los datos.
+     *
+     * @return Una instancia de SecretKey que representa la clave secreta.
+     */
     private static SecretKey getSecretKey() {
         byte[] keyBytes = CLAVE_SECRETA.getBytes();
         return new SecretKeySpec(keyBytes, ALGORITMO);
     }
 
+    /**
+     * Cifra los datos utilizando la clave secreta.
+     *
+     * @param data Los datos a cifrar.
+     * @return Una cadena cifrada en Base64.
+     */
     public static String cifrar(String data) {
         byte[] encryptedBytes = null;
         try {
@@ -30,6 +43,12 @@ public class Seguridad {
         return Base64.getEncoder().encodeToString(encryptedBytes);
     }
 
+    /**
+     * Descifra los datos cifrados utilizando la clave secreta.
+     *
+     * @param encryptedData Los datos cifrados en Base64.
+     * @return Una cadena con los datos descifrados.
+     */
     public static String descifrar(String encryptedData) {
         byte[] decryptedBytes = null;
         try {
